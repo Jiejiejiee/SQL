3c47089a0c0d88788d43236eb4cd15f56943dcaf
liwenjie <liwenjie@glbank.com>
2024-02-26 15:28:42 +0800

    SQL210

--- NowCode.sql
+++ NowCode.sql
@@ -16,20 +16,33 @@ where b.salary in (select max(salary) max_ssecondary_salaryalary_2
 
 -- 206
 select uni.dept_no, uni.emp_no, max_salary.salary
-from
-    (select d.dept_no, s.emp_no, s.salary
-     from dept_emp d join salaries s
-     on d.emp_no = s.emp_no
-     and d.to_date = '9999-01-01'
-     and s.to_date = '9999-01-01'
-    ) as uni, /* 部门编号,员工编号,当前薪水 */
-    (select d.dept_no, max(s.salary) as salary
-     from dept_emp d join salaries s
-     on d.emp_no = s.emp_no
-     and d.to_date = '9999-01-01'
-     and s.to_date = '9999-01-01'
-     group by d.dept_no
-    ) as max_salary /* 部门编号,当前最高薪水 */
+from (select d.dept_no, s.emp_no, s.salary
+      from dept_emp d
+               join salaries s
+                    on d.emp_no = s.emp_no
+                        and d.to_date = '9999-01-01'
+                        and s.to_date = '9999-01-01') as uni, /* 部门编号,员工编号,当前薪水 */
+     (select d.dept_no, max(s.salary) as salary
+      from dept_emp d
+               join salaries s
+                    on d.emp_no = s.emp_no
+                        and d.to_date = '9999-01-01'
+                        and s.to_date = '9999-01-01'
+      group by d.dept_no) as max_salary /* 部门编号,当前最高薪水 */
 where uni.salary = max_salary.salary
-and uni.dept_no = max_salary.dept_no
+  and uni.dept_no = max_salary.dept_no
 order by uni.dept_no;
+
+-- 205
+select a.emp_no, b.emp_no
+from dept_emp a
+         left join dept_manager b
+                   on a.dept_no = b.dept_no
+where a.emp_no not in (select emp_no from dept_manager);
+
+-- 210
+select title, avg(s.salary)
+from titles t
+         left join salaries s
+                   on t.emp_no = s.emp_no
+group by title;
