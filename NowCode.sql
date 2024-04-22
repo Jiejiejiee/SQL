1362a10b57490e68bc25c2a2c53d3fb85527113c
liwenjie <liwenjie@glbank.com>
2024-04-22 09:47:13 +0800

    219

--- NowCode.sql
+++ NowCode.sql
@@ -1,18 +1,10 @@
--- 212
-select max(salary) secondary_salary
-from salaries
-where salary != (select max(salary) max_salary from salaries);
+-- 205
+select a.emp_no, b.emp_no
+from dept_emp a
+         left join dept_manager b
+                   on a.dept_no = b.dept_no
+where a.emp_no not in (select emp_no from dept_manager);
 
-select b.emp_no,
-       b.salary,
-       a.last_name,
-       a.first_name
-from employees a
-         inner join
-     salaries b on a.emp_no = b.emp_no
-where b.salary in (select max(salary) max_ssecondary_salaryalary_2
-                   from salaries
-                   where salary != (select max(salary) max_salary from salaries));
 
 -- 206
 select uni.dept_no, uni.emp_no, max_salary.salary
@@ -33,12 +25,6 @@ where uni.salary = max_salary.salary
   and uni.dept_no = max_salary.dept_no
 order by uni.dept_no;
 
--- 205
-select a.emp_no, b.emp_no
-from dept_emp a
-         left join dept_manager b
-                   on a.dept_no = b.dept_no
-where a.emp_no not in (select emp_no from dept_manager);
 
 -- 210
 select title, avg(s.salary)
@@ -47,24 +33,23 @@ from titles t
                    on t.emp_no = s.emp_no
 group by title;
 
--- 218
-select de.dept_no, e.emp_no, salary
-from employees e
-         left join dept_emp de
-                   on e.emp_no = de.emp_no
-    -- left join dept_manager dm
-    -- on de.dept_no = dm.dept_no
-         left join salaries s
-                   on e.emp_no = s.emp_no
-where e.emp_no not in (select emp_no from dept_manager);
 
--- 217
-select emp_no, salary, rank() over (order by salary desc ) as value
-from salaries;
--- 如果salary相同，再按照emp_no升序排列
-select emp_no, salary, dense_rank() over (order by salary desc ) as value
+-- 212
+select max(salary) secondary_salary
 from salaries
-order by salary desc, emp_no;
+where salary != (select max(salary) max_salary from salaries);
+
+select b.emp_no,
+       b.salary,
+       a.last_name,
+       a.first_name
+from employees a
+         inner join
+     salaries b on a.emp_no = b.emp_no
+where b.salary in (select max(salary) max_ssecondary_salaryalary_2
+                   from salaries
+                   where salary != (select max(salary) max_salary from salaries));
+
 
 -- 215
 select s1.emp_no, (s2.salary - s1.salary) as growth
@@ -90,6 +75,30 @@ from (select e.emp_no, s.salary
       where s.to_date = '9999-01-01') b -- 现在工资表
      on a.emp_no = b.emp_no
 order by growth;
+
+
+-- 217
+select emp_no, salary, rank() over (order by salary desc ) as value
+from salaries;
+-- 如果salary相同，再按照emp_no升序排列
+select emp_no, salary, dense_rank() over (order by salary desc ) as value
+from salaries
+order by salary desc, emp_no;
+
+
+-- 218
+select de.dept_no, e.emp_no, salary
+from employees e
+         left join dept_emp de
+                   on e.emp_no = de.emp_no
+    -- left join dept_manager dm
+    -- on de.dept_no = dm.dept_no
+         left join salaries s
+                   on e.emp_no = s.emp_no
+where e.emp_no not in (select emp_no from dept_manager);
+
+
+-- 219
 
 -- 253
 -- Todo:left join 出现数据丢失的情况
